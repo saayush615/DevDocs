aabad4cc4a00e431ec99eea86ec9829202f0f713
Aayush singh <saayush615@gmail.com>
2026-08-12 23:42:47 +0530

    feat(prisma): initialize Prisma setup with User and Post models.

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -2,7 +2,7 @@ import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist/**', 'node_modules/**'] },
+  { ignores: ['dist/**', 'node_modules/**', 'src/generated/**', 'prisma.config.ts'] },
   tseslint.configs.recommended, // ESLint recommended rules + TypeScript recommended rules.
   eslintConfigPrettier, // it disables any formatting rules that conflict with Prettier.
 );
