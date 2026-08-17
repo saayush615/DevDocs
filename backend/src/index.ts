0458e16e6c7b188d51029a3ff86ca2316c9b00f2
Aayush singh <saayush615@gmail.com>
2026-08-17 19:54:34 +0530

    feat: add better-auth route and configure prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,24 @@
+import cors from 'cors';
 import express, { type Express, type Request, type Response } from 'express';
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth.js';
 
 const app: Express = express();
-const port = 3000;
+const port = 3001;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
+    credentials: true,
+  }),
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth)); // better auth route
+
+app.use(express.json());
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Welcome to DevDocs');
 });
 
 app.listen(port, () => {
---
+++ backend/src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma.js";
+
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.BETTER_AUTH_URL ?? 'http://localhost:3001',
+  trustedOrigins: [process.env.FRONTEND_URL ?? 'http://localhost:3000'],
+  advanced: {
+    cookiePrefix: 'devdocs',
+    useSecureCookies: false,
+  },
+});
