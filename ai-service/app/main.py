382207b2d6b894927765f476ee0a8099fceb5fd8
Aayush singh <saayush615@gmail.com>
2026-09-03 18:31:26 +0530

    feat: add heath route and a simple ingest test route

--- ai-service/app/main.py
+++ ai-service/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from routers.ingest import router as ingest_router # Python sees app as the top-level package( running uvicorn app.main:app)
 
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to devdocs ai service"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+app.include_router(ingest_router)
