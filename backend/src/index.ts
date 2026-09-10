6d15c01933ab8f031ded5a0e7829b64eea0b2207
Aayush singh <saayush615@gmail.com>
2026-09-10 14:44:22 +0530

    feat: add document, conversation & chat endpoint(all the required routes)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,9 @@ import express, { type Express, type Request, type Response } from 'express';
 import { toNodeHandler } from 'better-auth/node';
 import { auth } from './lib/auth.js';
 import { notFound, errorHandler } from './middleware/errorHandler.js';
+import { documentsRouter } from './routes/documents.route.js';
+import { conversationsRouter } from './routes/conversations.route.js';
+import { chatRouter } from './routes/chat.route.js';
 
 const app: Express = express();
 const port = 3001;
@@ -19,9 +22,13 @@ app.all('/api/auth/*splat', toNodeHandler(auth)); // better auth route
 
 app.use(express.json());
 
-app.get('/', (_req: Request, res: Response) => {
+app.get('/api', (_req: Request, res: Response) => {
   res.send('Welcome to DevDocs');
 });
+
+app.use('/api/document', documentsRouter);
+app.use('/api/conversation', conversationsRouter);
+app.use('/api/chat', chatRouter);
 
 app.use(notFound);
 app.use(errorHandler);
